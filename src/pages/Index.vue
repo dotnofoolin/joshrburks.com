8514fa185d16ec9c05569a9889458fef2faa7786
Josh Burks <dotnofoolin@gmail.com>
2021-05-04 10:55:23 -0500

    Bulk of About/Index page done.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,123 @@
 <template>
   <Layout>
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-    <button class="button is-primary"><i data-feather="circle"></i>Click me</button>
-
-    <div class="box"><p>Some cool content</p></div>
-    <i data-feather="circle"></i>
-
+    <div class="columns mt-4 ml-2 mr-2">
+      <div class="column is-full">
+        <div class="content">
+          <h1>Hello World!</h1>
+          <p>I'm Josh. I'm currently a Senior Software Developer at Textbook Technology. I'm also a husband to a wonderful woman I've known for more than 20 years. And I'm a dad, to three kids, which were all adopted. I spend my freetime with my family, or you might catch me out in the garage tinkering on my FJ60 Land Cruiser, or out on the trail enjoying the beauty of Arkansas. Sometimes I fire up the chainsaw, climb up on the tractor, and take care of my property.</p>
+        </div>
+      </div>
+    </div>
+    <div class="columns mt-4 ml-2 mr-2">
+      <div class="column is-half">
+        <div class="content">
+          <h3>History</h3>
+        </div>
+        <div class="timeline">
+          <header class="timeline-header">
+            <span class="tag is-info">Born</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">1981</p>
+              <p>Born in Little Rock, AR. Raised in Hot Spring County.</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-info">School</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">1986</p>
+              <p>Started at Bismarck Public Schools</p>
+            </div>
+          </div>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">1999</p>
+              <p>Graduated from Bismarck Public Schools</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-info">College</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">2003</p>
+              <p>Graduated from Henderson State University</p>
+              <p>Bachelor's of Science in Computer Science</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-info">Employment</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">2004</p>
+              <p>Moved to Little Rock, AR</p>
+            </div>
+          </div>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">2004 - 2013</p>
+              <p>Worked for Flake Wilkerson Market Insights / Market Strategies (now Escalent)</p>
+            </div>
+          </div>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">2013 - 2014</p>
+              <p>Worked for HealthSCOPE Benefits</p>
+            </div>
+          </div>
+          <div class="timeline-item">
+            <div class="timeline-marker"></div>
+            <div class="timeline-content">
+              <p class="heading">2014</p>
+              <p>Started working for Textbook Technology</p>
+            </div>
+          </div>
+          <div class="timeline-header">
+            <span class="tag is-info">Present Day</span>
+          </div>
+        </div>
+      </div>
+      <div class="column is-half">
+        <div class="content">
+          <h3>Tech Stack</h3>
+          <p>Over the years I've worked with a variety of technologies. From C++ to Perl, to linux shell scripting, to Ruby, Javascript, and whatever comes next.</p>
+          <p>Some of my favorites:</p>
+          <ul>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Mac OS</li>
+            <li>Linux</li>
+            <li>Amazon Web Services (AWS)</li>
+            <li>Javascript</li>
+            <li>Vue.js</li>
+            <li>PostgreSQL</li>
+            <li>Git version control, and the git-flow branching model</li>
+          </ul>
+          <p>Tech I've worked with but don't favor:</p>
+          <ul>
+            <li>Perl</li>
+            <li>Python</li>
+            <li>Django</li>
+            <li>AngularJS</li>
+            <li>Angular (v2+)</li>
+            <li>Windows Server</li>
+            <li>VBA Scripting (mostly Access and Excel)</li>
+            <li>And a variety of proprietary business-specific languages and tech</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
